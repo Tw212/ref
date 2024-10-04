37fd4246abc49a68f1bc558cd1a27a359ee5e3eb
Tw212 <Tewe07001@utb.vaxjo.se>
2024-10-04 13:57:51 +0200

    ref

--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int a=10, b=20;
-BytPlats(ref a, b);
+/*int a=10, b=20;
+BytPlats(ref a, ref b);
 Console.WriteLine(a+" "+b);
 
 
-static void BytPlats(ref int a, int b){
+static void BytPlats(ref int a, ref int b){
+   int temp = a;
+   a=b;// a:10   b:20
+   b=temp;// a:20   b:20
+}*/
 
-}
+
+/*float a=7.5F;
+int b=2;
+BytPlats(ref a, ref b);
+Console.WriteLine(Math.Pow(a,b));
+
+
+static void BytPlats(ref float a, ref int b){
+  
+}*/
+
+using System.Runtime.Intrinsics.X86;
+
+Console.WriteLine("Skriv din längd och vikt så räknar vi ut din BMI");
+Console.ReadLine();
+float b = 0, input;
+float c = 0;
+float BMI = (b*b)/c;
+Console.WriteLine("din bmi är " + (BMI));
--- obj/Debug/net8.0/ref.AssemblyInfo.cs
+++ obj/Debug/net8.0/ref.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ref")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8794a274cd60754b8c8f5e43d07eec5b50c8587")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49945b6e9c2d008ac455512f491a5dbcdef1e457")]
 [assembly: System.Reflection.AssemblyProductAttribute("ref")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ref")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
